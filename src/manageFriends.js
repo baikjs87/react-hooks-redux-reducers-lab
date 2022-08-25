82cdd3e9160f93d447ae6d98165b94822ad4eb07
Jason Baik <baikjs87@gmail.com>
2022-08-24 22:59:09 -0400

    Done but test not working

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,20 @@
-export function manageFriends(state, action) {
-  // your code here
+const initialState = { friends: [] }
+
+export function manageFriends(state = initialState, action) {
+  switch (action.type) {
+    case "friends/add":
+      return { 
+        newFriend: [...friends, {
+          name: "Chrome Boi",
+          hometown: "NYC",
+          id: 1
+        }]
+      }
+    case "friends/remove":
+      return {
+        friendRemoved: [...friends].filter((friend) => friend.id === action.payload)
+      }
+    default:
+      return state
+  }
 }
